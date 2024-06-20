cd9ae68cb5bfbef68398958ca956e6d133fd099e
sanjay010911 <sanjaybenoy123@gmail.com>
2024-06-20 23:05:12 +0530

    Add project files.

---
+++ GenericMethodDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GenericMethodDemo
+{
+    public class Program
+    {
+        static void Swap<T>(T t1, T t2)
+        {
+            T temp;
+            Console.WriteLine("Before swapping t1 :" + t1 + " t2 :" + t2);
+            temp = t1;
+            t1 = t2;
+            t2 = temp;
+            Console.WriteLine("After swapping t1 :" + t1 + " t2 :" + t2);
+        }
+        static void Main(string[] args)
+        {
+            Swap<int>(56, 32);
+            Swap<string>("abc", "def");
+        }
+    }
+}
